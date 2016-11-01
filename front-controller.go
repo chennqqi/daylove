f109aed96dfca81327b68df4afa42a24dbe390de
ZhiFeng Hu <hufeng1987@gmail.com>
2016-11-01 11:37:27 +0800

    Fix typo of go-sql-driver
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- front-controller.go
+++ front-controller.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
-	_ "github.com/go-mysql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 	"html/template"
 	"net/http"
 	"strconv"
