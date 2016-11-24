93a0cb9d0b7e2a8100fd6d6bf91d9f3796cd2f7c
ZhiFeng Hu <hufeng1987@gmail.com>
2016-11-24 11:06:10 +0800

    Update the sql struct
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- sql/bak.sql
+++ sql/bak.sql
@@ -3,8 +3,10 @@ use daylove;
 CREATE TABLE `article` (
   `aid` int(11) NOT NULL AUTO_INCREMENT,
   `content` longtext CHARACTER SET utf8,
+  `images` JSON NOT NULL,
   `publish_time` datetime DEFAULT CURRENT_TIMESTAMP,
   `publish_status` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`aid`),
   FULLTEXT KEY `content` (`content`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
